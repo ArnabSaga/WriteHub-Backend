39b73957f3a8fb56c716e120f975433041a6e938
ArnabSaga <arnabdey15091@gmail.com>
2025-12-31 01:25:40 +0600

    Extending the User Model: Adding Additional User Fields

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
 import { toNodeHandler } from "better-auth/node";
-import { auth } from './lib/auth';
+import { auth } from "./lib/auth";
 
 import { PostRouter } from "./modules/post/post.route";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true,
+  })
+);
 
 app.all("/api/auth/*splat", toNodeHandler(auth));
 
